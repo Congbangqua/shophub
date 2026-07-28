094d5806d80c57c43df38db12ce7d4f73f8aabd1
Congbangqua <155067866+Congbangqua@users.noreply.github.com>
2026-07-28 17:36:34 +0700

    Update product.py

--- backend/models/product.py
+++ backend/models/product.py
@@ -12,6 +12,7 @@ class ProductDB(Base):
     description = Column(Text, nullable=False)
     image_path = Column(String(255), nullable=False)
     stock = Column(Integer, nullable=False, default=0)
+    discount_percent = Column(Integer, nullable=False, default=0)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(
         DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
