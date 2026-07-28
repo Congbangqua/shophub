65e1632f38a620f04e9f8785cf55aae3ecf95763
Congbangqua <155067866+Congbangqua@users.noreply.github.com>
2026-07-28 17:19:25 +0700

    Update productImages.js

--- frontend/src/data/productImages.js
+++ frontend/src/data/productImages.js
@@ -4,7 +4,7 @@ export const productImages = [
   { label: 'iPhone 15plus', path: '/images/iphone15plus.jpg' },
   { label: 'iPhone 15promax', path: '/images/iphone15promax.jpg' },
   { label: 'iPhone 16', path: '/images/iphone16.jpg' },
-  { label: 'iPhone 16pro', path: '/images/iphone16oro.jpg' },
+  { label: 'iPhone 16pro', path: '/images/iphone16pro.jpg' },
   { label: 'iPhone 16plus', path: '/images/iphone16plus.jpg' },
   { label: 'iPhone 17', path: '/images/iphone17.jpg' },
   { label: 'iPhone 17pro', path: '/images/iphone17pro.jpg' },
