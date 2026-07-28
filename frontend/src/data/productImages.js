c2000d8d929812c09cca1212a0ae8271e9c8b9f8
Congbangqua <155067866+Congbangqua@users.noreply.github.com>
2026-07-28 17:13:08 +0700

    Create productImages.js

---
+++ frontend/src/data/productImages.js
@@ -0,0 +1,12 @@
+export const productImages = [
+  { label: 'iPhone 15', path: '/images/iphone15.jpg' },
+  { label: 'iPhone 15pro', path: '/images/iphone15pro.jpg' },
+  { label: 'iPhone 15plus', path: '/images/iphone15plus.jpg' },
+  { label: 'iPhone 15promax', path: '/images/iphone15promax.jpg' },
+  { label: 'iPhone 16', path: '/images/iphone16.jpg' },
+  { label: 'iPhone 16pro', path: '/images/iphone16oro.jpg' },
+  { label: 'iPhone 16plus', path: '/images/iphone16plus.jpg' },
+  { label: 'iPhone 17', path: '/images/iphone17.jpg' },
+  { label: 'iPhone 17pro', path: '/images/iphone17pro.jpg' },
+  { label: 'iPhone 17promax', path: '/images/iphone17promax.jpg' },
+];
