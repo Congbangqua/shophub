dfc841c596bbd26aac01cb02f397a934b11ff3c6
Congbangqua <155067866+Congbangqua@users.noreply.github.com>
2026-07-31 18:45:46 +0700

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -3,18 +3,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
-STRIPE_SUCCESS_URL = os.environ.get("STRIPE_SUCCESS_URL")
-STRIPE_CANCEL_URL = os.environ.get("STRIPE_CANCEL_URL")
+STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
+STRIPE_SUCCESS_URL = os.getenv("STRIPE_SUCCESS_URL", "http://localhost:5173/payment/stripe/success")
+STRIPE_CANCEL_URL = os.getenv("STRIPE_CANCEL_URL", "http://localhost:5173/payment/stripe/cancel")
 
-PAYPAL_CLIENT_ID = os.environ.get("PAYPAL_CLIENT_ID")
-PAYPAL_CLIENT_SECRET = os.environ.get("PAYPAL_CLIENT_SECRET")
-PAYPAL_API_BASE = os.environ.get("PAYPAL_API_BASE")
-PAYPAL_RETURN_URL = os.environ.get("PAYPAL_RETURN_URL")
-PAYPAL_CANCEL_URL = os.environ.get("PAYPAL_CANCEL_URL")
+PAYPAL_CLIENT_ID = os.getenv("PAYPAL_CLIENT_ID")
+PAYPAL_CLIENT_SECRET = os.getenv("PAYPAL_CLIENT_SECRET")
+PAYPAL_RETURN_URL = os.getenv("PAYPAL_RETURN_URL", "http://localhost:5173/payment/paypal/success")
+PAYPAL_CANCEL_URL = os.getenv("PAYPAL_CANCEL_URL", "http://localhost:5173/payment/paypal/cancel")
+PAYPAL_API_BASE = os.getenv("PAYPAL_API_BASE", "https://api-m.sandbox.paypal.com")
 
-VNPAY_TMN_CODE = os.environ.get("VNPAY_TMN_CODE")
-VNPAY_HASH_SECRET = os.environ.get("VNPAY_HASH_SECRET")
-VNPAY_RETURN_URL = os.environ.get("VNPAY_RETURN_URL")
-VNPAY_BASE_URL = os.environ.get("VNPAY_BASE_URL")
+VNPAY_TMN_CODE = os.getenv("VNPAY_TMN_CODE")
+VNPAY_HASH_SECRET = os.getenv("VNPAY_HASH_SECRET")
+VNPAY_RETURN_URL = os.getenv("VNPAY_RETURN_URL", "http://localhost:5173/payment/vnpay/success")
+VNPAY_BASE_URL = os.getenv("VNPAY_BASE_URL", "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html")
+USD_TO_VND_RATE = 25000
 USD_TO_VND_RATE = 25000
