01ba3382b36686a5652bbd40bcf31e1d34f5ddb2
Congbangqua <155067866+Congbangqua@users.noreply.github.com>
2026-07-31 18:42:23 +0700

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -3,18 +3,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-STRIPE_SECRET_KEY = os.getenv("sk_test_51TxDNmFxYxE1Xw2Z2TN5jiAWwLJP2GqOEKbwLQJvo2Bh4QCWQFRsoFCHWRyECNrgvWwrTHLG3cXj1rKMRqVfuR0k0063lojDgp")
-STRIPE_SUCCESS_URL = os.getenv("https://shophub-qs3z.vercel.app/payment/stripe/success")
-STRIPE_CANCEL_URL = os.getenv("https://shophub-qs3z.vercel.app/payment/stripe/cancel")
+STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
+STRIPE_SUCCESS_URL = os.environ.get("STRIPE_SUCCESS_URL")
+STRIPE_CANCEL_URL = os.environ.get("STRIPE_CANCEL_URL")
 
-PAYPAL_CLIENT_ID = os.getenv("BAAjoV6LAWwiJ_1yPm7ZE73_V-emJdOmwqhKhzzh9FMM8imTg7JcgESz3I-iI5i8iCqV2tiohLeI-xvb98")
-PAYPAL_CLIENT_SECRET = os.getenv("EK3zDySGbN5hD70jBcBQHjvetu59qhJeVQBTQ4vi8viFC0FzU9d5gU9fy92q1p8MNv_VfI1V5Kl0R7Qz")
-PAYPAL_RETURN_URL = os.getenv("https://shophub-qs3z.vercel.app/payment/paypal/success")
-PAYPAL_CANCEL_URL = os.getenv("https://shophub-qs3z.vercel.app/payment/stripe/cancel")
-PAYPAL_API_BASE = os.getenv("https://api-m.sandbox.paypal.com")
+PAYPAL_CLIENT_ID = os.environ.get("PAYPAL_CLIENT_ID")
+PAYPAL_CLIENT_SECRET = os.environ.get("PAYPAL_CLIENT_SECRET")
+PAYPAL_API_BASE = os.environ.get("PAYPAL_API_BASE")
+PAYPAL_RETURN_URL = os.environ.get("PAYPAL_RETURN_URL")
+PAYPAL_CANCEL_URL = os.environ.get("PAYPAL_CANCEL_URL")
 
-VNPAY_TMN_CODE = os.getenv("FT4EZ7TS")
-VNPAY_HASH_SECRET = os.getenv("TQBWSNWJWKHTCWEUCKMFOWMUVKSIBRAT")
-VNPAY_RETURN_URL = os.getenv("https://shophub-qs3z.vercel.app/payment/vnpay/success")
-VNPAY_BASE_URL = os.getenv("https://sandbox.vnpayment.vn/paymentv2/vpcpay.html")
+VNPAY_TMN_CODE = os.environ.get("VNPAY_TMN_CODE")
+VNPAY_HASH_SECRET = os.environ.get("VNPAY_HASH_SECRET")
+VNPAY_RETURN_URL = os.environ.get("VNPAY_RETURN_URL")
+VNPAY_BASE_URL = os.environ.get("VNPAY_BASE_URL")
 USD_TO_VND_RATE = 25000
