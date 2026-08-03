1a40896c7ab85169b48112cb764ea1bbce3db7fa
Congbangqua <155067866+Congbangqua@users.noreply.github.com>
2026-08-03 18:51:41 +0700

    Update shipping.py

--- backend/schemas/shipping.py
+++ backend/schemas/shipping.py
@@ -1,1 +1,11 @@
+from pydantic import BaseModel
+from typing import Optional
 
+class ShippingFeeRequest(BaseModel):
+    to_district_id: int
+    to_ward_code: str
+    weight: int
+    insurance_value: Optional[int] = 0
+
+class ShippingFeeResponse(BaseModel):
+    fee: float
