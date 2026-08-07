75a049a3958286898bff0ba7b6c5f22a1b925b51
Congbangqua <155067866+Congbangqua@users.noreply.github.com>
2026-08-07 17:03:32 +0700

    Update shippingApi.js

--- frontend/src/api/shippingApi.js
+++ frontend/src/api/shippingApi.js
@@ -7,6 +7,29 @@ function authHeaders() {
 }
 
 export const shippingApi = {
+  async getProvinces() {
+    const response = await axiosClient.get('/shipping/provinces', {
+      headers: authHeaders(),
+    });
+    return response.data;
+  },
+
+  async getDistricts(provinceId) {
+    const response = await axiosClient.get('/shipping/districts', {
+      params: { province_id: provinceId },
+      headers: authHeaders(),
+    });
+    return response.data;
+  },
+
+  async getWards(districtId) {
+    const response = await axiosClient.get('/shipping/wards', {
+      params: { district_id: districtId },
+      headers: authHeaders(),
+    });
+    return response.data;
+  },
+
   async calculateFee({ to_district_id, to_ward_code, weight, insurance_value = 0 }) {
     const response = await axiosClient.post(
       '/shipping/calculate-fee',
